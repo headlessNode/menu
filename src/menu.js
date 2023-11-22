12bf93f4ba216d15e28082a1c88198ebde4ce59d
headlessNode <harriskhan047@outlook.com>
2023-11-22 22:10:38 +0500

    Initial setup for the project - add eslint - add prettier - add template html - add css file for style - add js file

---
+++ src/menu.js
@@ -0,0 +1,6 @@
+import './menuStyle.css';
+
+const wrapper = document.querySelector(".wrapper");
+const header = document.createElement("div");
+header.classList.add("header");
+wrapper.appendChild(header);
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/menu.js",
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
