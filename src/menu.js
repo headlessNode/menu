fa92046ffd1c2f314a75b774330b90eb334fa31d
headlessNode <harriskhan047@outlook.com>
2023-11-23 20:25:51 +0500

    Add svg and hover animation to menu btn

--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,10 @@
-import './menuStyle.css';
+import "./menuStyle.css";
 
-const wrapper = document.querySelector(".wrapper");
-const header = document.createElement("div");
-header.classList.add("header");
-wrapper.appendChild(header);
+// DOM
+const wrapper =
+  document.querySelector(".wrapper");
+const menuBtn =
+  document.createElement("button");
+menuBtn.classList.add("menu-btn");
+menuBtn.innerHTML ='<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" class="menu-icon" viewBox="0 0 40 60" xml:space="preserve"><switch><g><path id="btm-line" class="st0" d="M2 17.72h36"/><path id="mid-line" class="st0" d="M38 30H2"/><path id="top-line" class="st0" d="M2 42.28h36"/></g></switch></svg>';
+wrapper.appendChild(menuBtn);
