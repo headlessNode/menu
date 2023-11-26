627f4571209ed814fe24ca31f93ddcd5476f5a8f
headlessNode <harriskhan047@outlook.com>
2023-11-26 13:23:15 +0500

    Add menu items - add animation to menu items - fix the animation for hide menu btn

--- src/menu.js
+++ src/menu.js
@@ -51,6 +51,29 @@ menu.classList.add('menu');
 menu.classList.add('menu-remove');
 const menuTop = document.createElement('div');
 menuTop.classList.add('menu-top');
+const menuItemsContainer = document.createElement('menuItenContainer');
+menuItemsContainer.classList.add('menu-items-container');
+const menuItemOne = document.createElement('p');
+menuItemOne.textContent = "Home";
+const menuItemTwo = document.createElement('p');
+menuItemTwo.textContent = "Feedback";
+const menuItemThree = document.createElement('p');
+menuItemThree.textContent = "Settings";
+const menuItemFour = document.createElement('p');
+menuItemFour.textContent = "Logout";
+
+
+const menuPic = document.createElement('div');
+menuPic.classList.add('menu-pic');
+menuPic.innerHTML = '<svg fill="#000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg"><path d="M16 15.503A5.041 5.041 0 1 0 16 5.42a5.041 5.041 0 0 0 0 10.083zm0 2.215c-6.703 0-11 3.699-11 5.5v3.363h22v-3.363c0-2.178-4.068-5.5-11-5.5z"/></svg>';
+menuItemsContainer.appendChild(menuItemOne);
+menuItemsContainer.appendChild(menuItemTwo);
+menuItemsContainer.appendChild(menuItemThree);
+menuItemsContainer.appendChild(menuItemFour);
+menuItemsContainer.appendChild(menuPic);
+
+menuTop.appendChild(menuItemsContainer);
+
 const menuBottom = document.createElement('div');
 menuBottom.classList.add('menu-btm');
 const menuModal = document.createElement('div');
