74065921cccc7f95dd1c789f3669585083598579
headlessNode <harriskhan047@outlook.com>
2023-11-26 11:36:07 +0500

    Add a modal to the menu - add animation to the modal - remove hide menu btn from main page and add it to the menu - remove the redundant css properties and classes

--- src/menu.js
+++ src/menu.js
@@ -2,27 +2,26 @@ import "./menuStyle.css";
 
 // DOM
 const wrapper = document.querySelector(".wrapper");
-
+// PAGE HEADER
 const header = document.createElement('div');
 header.classList.add('header');
 wrapper.appendChild(header);
-
+// MENU BTN
 const menuBtn = document.createElement("button");
 menuBtn.classList.add("menu-btn");
-menuBtn.classList.add('menu-btn-show');
 menuBtn.innerHTML ='<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" class="menu-icon" viewBox="0 0 40 60" xml:space="preserve"><switch><g><path id="btm-line" class="st0" d="M2 17.72h36"/><path id="mid-line" class="st0" d="M38 30H2"/><path id="top-line" class="st0" d="M2 42.28h36"/></g></switch></svg>';
 header.appendChild(menuBtn);
-
+// HEADER HEADING
 const heading = document.createElement('h1');
 heading.textContent = "Menu Design"
 header.appendChild(heading);
-
+// MAIN BODY
 const mainBody = document.createElement('div');
 mainBody.classList.add('main-body');
 const mainBodyHeader = document.createElement('h2');
 mainBodyHeader.classList.add('main-body-header');
 mainBodyHeader.textContent = 'Lorem Ipsum';
-
+// MAIN BODY CONTENT
 const mainBodyContent = document.createElement('div');
 mainBodyContent.classList.add('main-body-content');
 
@@ -45,6 +44,8 @@ mainBody.appendChild(mainBodyContent);
 
 wrapper.appendChild(mainBody);
 
+// MENU
+
 const menu = document.createElement('div');
 menu.classList.add('menu');
 menu.classList.add('menu-remove');
@@ -52,24 +53,34 @@ const menuTop = document.createElement('div');
 menuTop.classList.add('menu-top');
 const menuBottom = document.createElement('div');
 menuBottom.classList.add('menu-btm');
+const menuModal = document.createElement('div');
+menuModal.classList.add('menu-modal');
+
 menu.appendChild(menuTop);
 menu.appendChild(menuBottom);
-wrapper.appendChild(menu);
+menu.appendChild(menuModal);
+
 
 const hideMenuBtn = document.createElement('button');
 hideMenuBtn.classList.add('hide-menu-btn');
 hideMenuBtn.classList.add('remove-hide-btn');
 hideMenuBtn.innerHTML = '<svg class="cross-icon" xml:space="preserve" viewBox="0 0 40 40"><switch><g stroke="#fff" stroke-linecap="round" stroke-miterlimit="10"><path d="M10 29.19 30 9"/><path fill="#fff" d="M30 29.19 10 9"/></g></switch></svg>';
-wrapper.appendChild(hideMenuBtn);
+menu.appendChild(hideMenuBtn);
+
+wrapper.appendChild(menu);
 
 function showMenu(){
   
+  // main menu container
   menu.classList.remove('menu-hide-visually');
   menu.classList.remove('menu-remove');
-  menuBtn.classList.remove('menu-btn-show');
-  menuBtn.classList.add('menu-btn-hide');
+  // menu top
   menuTop.classList.remove("hide-menu-top");
+  // menu bottom
   menuBottom.classList.remove("hide-menu-btm");
+  // menu modal
+  menuModal.classList.remove("menu-modal-hide");
+  
   menu.classList.add('menu-show');
   hideMenuBtn.classList.remove('remove-hide-btn');
   hideMenuBtn.classList.add('show-hide-btn');
@@ -80,7 +91,7 @@ function hideMenu(){
   menu.classList.remove('menu-show');
   menuTop.classList.add("hide-menu-top");
   menuBottom.classList.add("hide-menu-btm");
-
+  menuModal.classList.add("menu-modal-hide");
   menu.classList.add('menu-hide-visually');
 
   hideMenuBtn.classList.remove('show-hide-btn');
@@ -88,7 +99,6 @@ function hideMenu(){
   
   setTimeout(()=>{
     menu.classList.add('menu-remove');
-    menuBtn.classList.remove('menu-btn-hide');
     menuBtn.classList.add('menu-btn-show');
   }, 1000);
 
